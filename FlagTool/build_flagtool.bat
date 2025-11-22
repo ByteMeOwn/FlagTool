445704e7feb5e40b3e8ed428e2619ea4b9926f54
Nosliaht <thailson-games-2002@hotmail.com>
2025-11-21 21:17:42 -0300

    build_flagtool.bat added and font chenging

---
+++ FlagTool/build_flagtool.bat
@@ -0,0 +1,14 @@
+@echo off
+pyinstaller --onefile --windowed --name FlagTool --icon ico.ico FlagTool.py
+
+REM Caminho final do executável
+set exe_dir=dist
+
+REM Copiar arquivos extras para pasta do executável
+copy "interface.ini" "%exe_dir%"
+copy "flags.ini" "%exe_dir%"
+copy "color.ini" "%exe_dir%"
+copy "font.ini" "%exe_dir%"
+copy "ico.ico" "%exe_dir%"
+
+pause
